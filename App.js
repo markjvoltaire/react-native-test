d05f126dc2f583ecbcea16490fa52fc5154ffd62
Mark Voltaire <markjvoltaire@gmail.com>
2022-02-05 17:22:54 -0500

    React Native Blank Template

--- App.js
+++ App.js
@@ -1,31 +1,10 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            borderBottomColor: "black",
-            borderWidth: 0.34,
-            padding: 10,
-            width: 200,
-            borderRadius: 20,
-          }}
-        />
-        <Button title="Add" />
-        <View></View>
-      </View>
+    <View>
+      <Text></Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
